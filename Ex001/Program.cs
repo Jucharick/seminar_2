5aa910cd77246a523a74c89a7f8ad6d04529fb6f
Julia Ch <juliecha@yandex.ru>
2022-10-04 20:27:25 +0300

    Add Ex001 - random

---
+++ Ex001/Program.cs
@@ -0,0 +1,17 @@
+﻿// int[] array = { 10, 11, 12, 14, 99 };
+// Console.WriteLine(array);
+
+int n = new Random().Next (10, 99);
+Console.WriteLine(n);
+int first = n/10;
+int second = n%10;
+Console.WriteLine(first);
+Console.WriteLine(second);
+if (first > second)
+{
+    Console.WriteLine("max = "+ first);
+}
+else
+{
+    Console.WriteLine("max = "+ second);
+}
