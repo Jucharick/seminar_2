51542494c119a248a886d0311cd93a24bbd79aa4
Julia Ch <juliecha@yandex.ru>
2022-10-04 21:03:22 +0300

    Add Ex012

---
+++ Ex012/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое целое число:");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:");
+int second = Convert.ToInt32(Console.ReadLine());
+if (first % second == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно");
+}
