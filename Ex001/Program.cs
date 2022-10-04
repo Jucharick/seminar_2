a95e1d7cccfafb7cf23afffd4668a13b337b9b40
Julia Ch <juliecha@yandex.ru>
2022-10-04 20:28:43 +0300

    UPD Ex001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,7 +1,4 @@
-﻿// int[] array = { 10, 11, 12, 14, 99 };
-// Console.WriteLine(array);
-
-int n = new Random().Next (10, 99);
+﻿int n = new Random().Next (10, 99);
 Console.WriteLine(n);
 int first = n/10;
 int second = n%10;
