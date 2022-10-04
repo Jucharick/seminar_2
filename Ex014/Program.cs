0b299e2b0d391788b0d6f943489f30fc7e73998a
Julia Ch <juliecha@yandex.ru>
2022-10-04 21:29:32 +0300

    Add Ex0014 and Ex0016

---
+++ Ex014/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите целое число:");
+int first = Convert.ToInt32(Console.ReadLine());
+
+if (first == 0 )
+{
+    Console.WriteLine("Ноль делить нельзя");
+}
+else
+{
+    if (first % 7 == 0 && first % 23 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+}
---
+++ Ex016/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое число:");
+int first = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите второе целое число:");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (first * first == second | second * second == first)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
