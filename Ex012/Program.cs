6f747c7bc54f2df501fd5621a4f5122c556f903b
Julia Ch <juliecha@yandex.ru>
2022-10-04 21:15:09 +0300

    UPD Ex012

--- Ex012/Program.cs
+++ Ex012/Program.cs
@@ -2,11 +2,18 @@
 int first = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе целое число:");
 int second = Convert.ToInt32(Console.ReadLine());
-if (first % second == 0)
+if (first == 0 | second == 0)
 {
-    Console.WriteLine("Кратно");
+    Console.WriteLine("На ноль делить нельзя");
 }
 else
 {
-    Console.WriteLine("Не кратно");
+    if (first % second == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
 }
