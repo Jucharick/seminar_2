8f85714e2a335cc701d9f14cab0f56a03e9761dd
Julia Ch <juliecha@yandex.ru>
2022-10-04 20:46:43 +0300

    Add Ex011

---
+++ Ex011/Program.cs
@@ -0,0 +1,7 @@
+﻿int n = new Random().Next (100, 1000); // 1000 не включается
+Console.WriteLine(n);
+int first = n/100;
+int third = n%10;
+Console.WriteLine(first);
+Console.WriteLine(third);
+Console.WriteLine(first*10+third);
