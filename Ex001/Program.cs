28148214abcdc750ccfcb58abe97ea18b2769a73
Julia Ch <juliecha@yandex.ru>
2022-10-29 21:13:26 +0300

    UPD Ex001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,15 +1,33 @@
-﻿int n = new Random().Next (10, 99);
-Console.WriteLine(n);
-int first = n/10;
-int second = n%10;
-Console.WriteLine(first);
-Console.WriteLine(second);
-if (first > second)
+﻿int n = new Random().Next(10, 99);
+Console.WriteLine($"случайное число: {n}");
+// int first = n/10;
+// int second = n%10;
+// Console.WriteLine(first);
+// Console.WriteLine(second);
+// if (first > second)
+// {
+//     Console.WriteLine("max = "+ first);
+// }
+// else
+// {
+//     Console.WriteLine("max = "+ second);
+// }
+// // 
+
+int MaxDigit(int num)
 {
-    Console.WriteLine("max = "+ first);
+    int firstDigit = n / 10;
+    int secondDigit = n % 10;
+
+    if (firstDigit == secondDigit) return -1;
+    return firstDigit > secondDigit ? firstDigit : secondDigit; // firstDigit больше secondDigit ? да - возвращает firstDigit, нет - возвращает secondDigit
 }
-else
+
+bool IsEqualDigit(int num1, int num2)
 {
-    Console.WriteLine("max = "+ second);
+    return num1 == num2;
 }
-// 
+
+int maxDigit = MaxDigit(n);
+
+Console.WriteLine($"Наибольшая цифра числа {n} => {maxDigit}");
